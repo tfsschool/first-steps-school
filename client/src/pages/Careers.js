c4b72cbbc88ca465543da57ed3a35e6e4edf51e9
Jiand Baloch <tfss.manage@gmail.com>
2026-01-02 19:35:36 +0500

    fix: update axios import in Careers.js

--- client/src/pages/Careers.js
+++ client/src/pages/Careers.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import axios from 'axios';
+import axios from '../config/axios';
 import { API_ENDPOINTS } from '../config/api';
 import { useAuth } from '../context/AuthContext';
 import SEO from '../components/SEO';
